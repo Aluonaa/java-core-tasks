50f76943b4301686fb444c5d1fc7a07e93f51fce
Strelets.A <Strelets.A@digdes.com>
2022-11-10 14:13:27 +0300

    add: Глава 1: выполнены 10, 14, исправлено 8

--- src/main/java/com/digdes/crp/javacoretasks/chapter1/Exercise8.java
+++ src/main/java/com/digdes/crp/javacoretasks/chapter1/Exercise8.java
@@ -3,13 +3,13 @@ package com.digdes.crp.javacoretasks.chapter1;
 import java.util.Scanner;
 
 public class Exercise8 {
+    /** Напишите программу, вводящую символьную строку и выводящую все ее непустые подстроки. **/
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String stroka = scanner.nextLine();
-        int length = stroka.length();
-        for (int i = 0; i < length; i++) {
-            stroka = stroka.replaceAll(" ", "");
+        String[] words = stroka.replaceAll("[\\s]{2,}", " ").split(" ");
+        for (String word : words) {
+            System.out.println(word);
         }
-        System.out.println(stroka);
     }
 }
