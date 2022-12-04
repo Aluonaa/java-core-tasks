0aa451353d7d6a55e2e4ecb08d39035f227f2f2c
Strelets.A <Strelets.A@digdes.com>
2022-12-04 21:19:43 +0300

    add: Глава 10: 5, 7, 17, 18, 19, 20, 21, 22, 23, 24

---
+++ src/main/java/com/digdes/crp/javacoretasks/chapter10/Exercise23.java
@@ -0,0 +1,13 @@
+package com.digdes.crp.javacoretasks.chapter10;
+
+public class Exercise23 {
+    /** Найдите ошибку в следующем фрагменте кода:
+    public class Stack {
+        public void push(Object newValue) {
+            synchronized (new ReentrantLock()) {
+            }
+        }
+    }
+     Решение: в блок synchronized незачем передавать блокировку, так
+     как эту функцию берет на себя сам synchronized **/
+}
