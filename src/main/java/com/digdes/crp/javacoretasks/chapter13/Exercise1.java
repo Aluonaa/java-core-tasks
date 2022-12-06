99e8e300de1336bc11d9723ad5eb7b4001b2c710
Strelets.A <Strelets.A@digdes.com>
2022-12-06 15:09:47 +0300

    add: Глава 13: 1, 5, 7

---
+++ src/main/java/com/digdes/crp/javacoretasks/chapter13/Exercise1.java
@@ -0,0 +1,32 @@
+package com.digdes.crp.javacoretasks.chapter13;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DecimalStyle;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+public class Exercise1 {
+
+    /** Напишите программу, демонстрирующую стили форматирования даты и
+    времени во французском, китайском и тайском представлении **/
+
+    public static void main(String[] args) {
+
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        Locale localeFrance = Locale.FRANCE;
+        Locale localeChina = Locale.CHINA;
+        Locale localeThailand =  Locale.forLanguageTag("th-TH-u-nu-thai");
+
+        DecimalStyle styleThailand = DecimalStyle.of(localeThailand);
+        DecimalStyle styleFrance = DecimalStyle.of(localeFrance);
+        DecimalStyle styleChina = DecimalStyle.of(localeChina);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+
+        System.out.println(formatter.withLocale(localeThailand).withDecimalStyle(styleThailand).format(dateTime));
+        System.out.println(formatter.withLocale(localeFrance).withDecimalStyle(styleFrance).format(dateTime));
+        System.out.println(formatter.withLocale(localeChina).withDecimalStyle(styleChina).format(dateTime));
+    }
+}
---
+++ src/main/java/com/digdes/crp/javacoretasks/chapter13/Exercise5.java
@@ -0,0 +1,15 @@
+package com.digdes.crp.javacoretasks.chapter13;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Exercise5 {
+    /** Повторите предыдущее упражнение для наименований денежных единиц.
+     *  **/
+    public static void main(String[] args) {
+        Set<Currency> currencies = Currency.getAvailableCurrencies();
+        for(Currency currency: currencies){
+            System.out.println(currency.getDisplayName());
+        }
+    }
+}
