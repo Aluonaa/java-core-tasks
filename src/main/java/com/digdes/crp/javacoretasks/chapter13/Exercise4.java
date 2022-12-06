8d1663419366c5b7f36063fb5182bf243e72fa4c
Strelets.A <Strelets.A@digdes.com>
2022-12-06 15:10:08 +0300

    fix: Глава 13: 4

--- src/main/java/com/digdes/crp/javacoretasks/chapter13/Exercise4.java
+++ src/main/java/com/digdes/crp/javacoretasks/chapter13/Exercise4.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class Exercise4 {
     /** Напишите программу, выводящую названия всех языков из региональных настроек
-    вашей виртуальной машины JVM на всех имеющихся языках. Отсортируйте их, исключив дубликаты. **/
+    вашей виртуальной машины JVM на всех имеющихся языках. Отсортируйте их, исключив дубликаты. **/
 
     public static void main(String[] args) {
         Set<String> languages = Arrays.stream(Locale.getISOLanguages())
